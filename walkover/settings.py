e18b94f2bdf29058e74d05fb56ddb37e688c3e28
Naman <namanjain9501@gmail.com>
2022-05-20 12:16:50 +0530

    postgresq

--- walkover/settings.py
+++ walkover/settings.py
@@ -78,26 +78,16 @@ WSGI_APPLICATION = 'walkover.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'd82cdbt3bhd34f',
-        'User': 'wliksutieaezyc',
-        'Password': '0c8b1f3b21e619334a433f2ab1926570c2f4d9536bac2c4b6241b57931a38b9c',
-        'HOST': 'ec2-44-195-169-163.compute-1.amazonaws.com',
-        'PORT': '5432',
-
-        
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
@@ -142,7 +132,7 @@ AUTH_USER_MODEL = 'docplatform.User'
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
